a5c5e7464c5d44d080dfc3f88e5f44bd2bb7c250
Uzair <uzairkhan907503@gmail.com>
2023-04-07 15:10:01 -0700

    basic column learned

--- app/src/main/java/com/example/jetpacklearning/MainActivity.kt
+++ app/src/main/java/com/example/jetpacklearning/MainActivity.kt
@@ -4,49 +4,46 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpacklearning.ui.theme.JetpackLearningTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        Color.Green
-                    ),
-                verticalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Text("Uzair")
-                Text(text = "Khan")
-            }
+            SetView()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SetView() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .width(300.dp)
+            .fillMaxHeight(.70f)
+            .background(
+                Color.Green
+            ),
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text("Uzair")
+        Text(text = "Khan")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackLearningTheme {
-        Greeting("Android")
+        SetView()
     }
 }
