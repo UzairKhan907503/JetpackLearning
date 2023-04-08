6a6c4ed6e399bbd13418f5f8746420aa47723881
Uzair <uzairkhan907503@gmail.com>
2023-04-07 17:21:08 -0700

    learned image card

--- app/src/main/java/com/example/jetpacklearning/MainActivity.kt
+++ app/src/main/java/com/example/jetpacklearning/MainActivity.kt
@@ -3,47 +3,84 @@ package com.example.jetpacklearning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.jetpacklearning.ui.theme.JetpackLearningTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SetView()
+            val painter = painterResource(id = R.drawable.sunset)
+            val description = "Beautiful Sun set"
+            val title = "An incredible Sun set that can blow your mind"
+            Box(modifier = Modifier
+                .fillMaxWidth(.5f)
+                .padding(16.dp)) {
+                ImageCard(painter = painter, contentDescription = description, title = title)
+            }
         }
     }
 }
 
 @Composable
-fun SetView() {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .width(300.dp)
-            .fillMaxHeight(.70f)
-            .background(
-                Color.Green
-            ),
-        verticalArrangement = Arrangement.SpaceEvenly
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
     ) {
-        Text("Uzair")
-        Text(text = "Khan")
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.FillBounds
+            )
+            Box(modifier = Modifier.fillMaxSize().background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color.Transparent,
+                        Color.Black
+                    ),
+                    startY = 300f
+                )
+            ))
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
+        }
+
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetpackLearningTheme {
-        SetView()
-    }
+//    ImageCard()
 }
