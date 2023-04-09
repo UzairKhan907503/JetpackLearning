5f80549ff95695d94206a1195ec9d4febc8bc5cf
Uzair <uzairkhan907503@gmail.com>
2023-04-09 12:49:15 -0700

    In this lesson lazy column was learned

--- app/src/main/java/com/example/jetpacklearning/MainActivity.kt
+++ app/src/main/java/com/example/jetpacklearning/MainActivity.kt
@@ -5,13 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpacklearning.ui.theme.JetpackLearningTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +32,36 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SetView() {
+    LazyColumn {
+        items(20) {
+            Text(
+                text = "Item $it",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 24.dp)
+            )
+        }
+    }
+}
 
+@Composable
+fun LazyColumnWithList() {
+    LazyColumn() {
+        itemsIndexed(listOf("This", "is", "Jetpack", "Compose")) { index, item ->
+            Text(
+                text = item,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 24.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
