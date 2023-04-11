b285bd701f0d73be908f8e676e0407bae524ce33
Uzair <uzairkhan907503@gmail.com>
2023-04-11 14:28:05 -0700

    In this lecture following things were learned and updated kotlin version in gradle for constraintlayout learned constraintlayout, guidelines, chains

--- app/src/main/java/com/example/jetpacklearning/MainActivity.kt
+++ app/src/main/java/com/example/jetpacklearning/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.checkScrollableContainerConstraints
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import com.example.jetpacklearning.ui.theme.JetpackLearningTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +31,40 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SetView() {
+    val constraints = ConstraintSet {
+        val greenBox = createRefFor("greenBox")
+        val redBox = createRefFor("redBox")
+        val guideLine = createGuidelineFromTop(.5f)
+        constrain(greenBox) {
+            top.linkTo(guideLine)
+            start.linkTo(parent.start)
+            width = Dimension.value(100.dp)
+            height = Dimension.value(100.dp)
 
+        }
+        constrain(redBox) {
+            top.linkTo(parent.top)
+            start.linkTo(greenBox.end)
+            width = Dimension.value(100.dp)
+            height = Dimension.value(100.dp)
+        }
+        createHorizontalChain(greenBox, redBox, chainStyle = ChainStyle.Packed)
+    }
+    ConstraintLayout(
+        constraintSet = constraints,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier
+                .background(Color.Green)
+                .layoutId("greenBox")
+        )
+        Box(
+            modifier = Modifier
+                .background(Color.Red)
+                .layoutId("redBox")
+        )
+    }
 }
 
 @Preview(showBackground = true)
